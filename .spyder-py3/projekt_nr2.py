c9f701c4a85562a63fe27e2268db0aa2dc5fb7fa
Jakub Garwoła <61791777+JGarwola@users.noreply.github.com>
2020-06-04 20:56:30 +0200

    Update projekt_nr2.py
    dodałem wstępnie periodyczność i budowanie walca jenocześnie w górę. Góra faktycznie pasuje do dołu, ale wciąż koncówki są zbyt wyboiste, przez co jest zbyt małe sigma.

--- .spyder-py3/projekt_nr2.py
+++ .spyder-py3/projekt_nr2.py
@@ -1,16 +1,50 @@
 # -*- coding: utf-8 -*-
-
+import math as m
 import numpy as np
 import random
+from scipy.stats import truncnorm as trunc
 import mogli
 
-N = 50
+N = 1000
 R = 10
 r = 0.7
-d_min = 1,4
+d_min = 1.4
 d_max = 2.1
 d_exp = 1.75
 element = 'B'
+
+#sprawdza czy atom jest w walcu
+def w_walcu(ato):
+    x =float(ato.pozycja[0])
+    y =float(ato.pozycja[1])
+    #z =float(ato.pozycja[2]) juz nie potrzebne
+    #if(z>=r and (x**2 +y**2 <= (R-r)**2)):
+    #zmienilem zeby walec tworzyl sie w obie strony bo wydaje mi sie 
+    #ze wtedy latwiej o periodycznosc bedzie zadbac
+    #uwzlednilem to przy sprawdzaniu h
+    if( (x**2 +y**2 <= (R-r)**2) ):
+        return True
+    else:
+        return False
+    
+    
+#LOSUJE ODLEGŁOŚĆ ZGODNIE Z ROZKŁADEM
+def losujd():
+    std = 0.7
+    a, b = (d_min - d_exp) /std, (d_max - d_exp) /std
+    k=trunc.rvs(d_min,d_max,loc=0,scale=1,size=1,random_state=None)
+    return k[0]
+
+#sprawdza czy odległość między atomami jest < odl
+#dodalem liczenie odleglosci w z modulo self.h_max na obrazku wygląda obiecująco
+def dystans(newato,ato,odl,h):
+    d2 = m.sqrt((float(newato.pozycja[0])-float(ato.pozycja[0]))**2 +
+                (float(newato.pozycja[1])-float(ato.pozycja[1]))**2 + 
+                (  (float(newato.pozycja[2])-float(ato.pozycja[2]))%h  )**2)
+    if(d2 <= odl):
+        return True
+    else:
+        return False
 
 class atom:
     def __init__(self, sigma=0, h=1, pozycja=np.array([])):
@@ -24,35 +58,86 @@ class atom:
                 + str(self.pozycja[1]) + " " + str(self.pozycja[2]) )
         
 class populacja():
-    def __init__(self, atomy=[]):
+    
+    def __init__(self, atomy=[], h_max = 2*r):
         self.atomy = atomy
+        self.h_max = 2*r
+        a0 = atom()
+        a0.pozycja = [format(0,'.5f'),format(0,'.5f'),format(r,'.5f')]
+        atomy.append(a0)
         
-        #TUTAJ BEDZIE MOZNA ZAIMPLEMENTOWAC LOSOWANIE POLOZEN
-        #NARAZIE WSPOLZEDNE PRZYPADKOWE, FORMAT WYMAGA 5 MIEJSC PO PRZECINKU
+        
+        #LOSOWANIE POLOZEN
+        
         for i in range(N):
-            x = format(random.uniform(-5,5),'.5f')
-            y = format(random.uniform(-5,5),'.5f')
-            z = format(random.uniform(-5,5),'.5f')
-            a = atom( pozycja = np.array( [x,y,z] ) )
+            while True:
+                za_blisko = 0
+                
+                if i>100:
+                    last = atomy[(int(random.uniform(0, i-100)))] 
+                else:
+                    last = atomy[(int(random.uniform(0, i)))] 
+                #wybiera losowy atom jako punkt odniesienia
+                #przy czym dodalem zeby nie wybieral 100 ostatnich
+                #to zapobuega tworzeniu nitek w pewnym stopniu
+                
+                #losuja współrzędne w ukłądzie sferycznym i przelicza na kartezjańskie
+                phi = (random.uniform(0,2*m.pi))
+                theta = (random.uniform(0,m.pi))
+                d = losujd()
+                x = d*m.sin(theta)*m.cos(phi) + float(last.pozycja[0])
+                y = d*m.sin(theta)*m.sin(phi) + float(last.pozycja[1])
+                z = d*m.cos(theta) + float(last.pozycja[2])
+                x = (format(x,'.5f'))
+                y = (format(y,'.5f'))
+                z = (format(z,'.5f'))
+                a = atom( pozycja = np.array( [x,y,z] ) )
+                
+                #sprawdzanie warunków
+                if(not w_walcu(a)):
+                    continue
+                for at in atomy:
+                    if(dystans(a,at,d_min,self.h_max)==True):
+                        za_blisko = 1
+                        break
+                        
+                if(za_blisko == 0):
+                    break
+                
+            #sprawdza h
+            #if(float(a.pozycja[2])+r > self.h_max):
+                #self.h_max = float(a.pozycja[2])+r
+            
+            #sprawdza h w przypadku budowania walca w dwie strony  
+            if (  abs(float(a.pozycja[2])-float(atomy[-1].pozycja[2])) +2*r  ) > self.h_max:
+                self.h_max = float(a.pozycja[2])-float(atomy[-1].pozycja[2]) +2*r 
+            
             self.atomy.append( a )
       
     #PRZEKSZTALCA LISTE ATOMY W TEKST GOTOWY DO WKLEJENIA DO PLIKU
     def dane(self):
-        tekst = str(N) + "\n" + "linia komentarza"
+        tekst = str(N+1) + "\n" + "linia komentarza"
         for element in self.atomy:
             tekst = ( tekst + "\n" + str(element) )
         return tekst
-    
-    #'{0:s},{1:f}x,{2:f}y,{3:f}z\n'.format('Ar',x[i],y[i],z)
     
     def zapisz(self,nazwa_pliku):
         fout = open(nazwa_pliku,"w+")
         fout.write(self.dane())
         fout.close()
-         
-p = populacja()
+
+
+p = populacja(h_max = 1.4)
+
+#parametry
+h = p.h_max
+V = m.pi*R*R*h
+Va = (4/3)* m.pi *r**3
+sigma = N*Va/V
+
 #print(p.dane())
+print("h =",h, "V =", 2*m.pi*r*h,"sigma =",sigma)
 p.zapisz("dane.xyz")
 
 molecules = mogli.read('dane.xyz')
-mogli.show(molecules[0])
+mogli.show(molecules[0],bonds_param=1)
